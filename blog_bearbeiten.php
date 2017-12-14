8a13165da0cb78482c3eb1d127c6b87c10e43402
Justin Tscherrig <justin.tscherrig@icloud.com>
2017-12-14 14:07:03 +0100

    Bloooooogs bearbeiten + erstellen

--- blog_bearbeiten.php
+++ blog_bearbeiten.php
@@ -1,22 +1,41 @@
 <?php
 
 $eid = $_GET['eid'];
+$blogId = $_GET['bid'];
 $entry = getEntry($eid);
 $title = $entry['title'];
 $content = $entry['content'];
+if(isset($_POST['inputContent'])&&isset($_POST['inputTitel'])){
+  $title = $_POST['inputTitel'];
+  $content = $_POST['inputContent'];
+  updateEntry($eid, $title, $content);
+}
+
 ?>
 
 
-<form action="<?php echo $_SERVER['PHP_SELF']."?function=updateEntry"; ?>" method="post">
+<form action="<?php echo $_SERVER['PHP_SELF']."?function=blog_bearbeiten&eid=$eid&bid=$blogId"; ?>" method="post">
 
   <div class="form-group" style="margin-top: 4em; width: 50%">
     <label for="inputTitel">Titel:</label>
-    <input type="text" name="inputTitel" id="inputTitel" placeholder="Titel" class="form-control" value="<?php echo $title?>" required>
+    <textarea rows="1" type="text" name="inputTitel" id="inputTitel" placeholder="Titel" class="form-control"><?php echo $title?></textarea>
   </div>
-</br>
   <div  style="margin-top: 4em;  width: 100%">
     <label for="inputContent">Text:</label>
-    <input type="text" name="inputContent" id="inputContent" placeholder="Content" value="<?php echo $content?>" required>
+    <br></br>
+    <textarea rows="35" cols="120" type="text" name="inputContent" id="inputContent" placeholder="Content"><?php echo $content?></textarea>
   </div>
-
+      <br></br>
+<input id="updateButton" class="btn btn-primary mt-2"  type="submit" value="Änderungen Speichern"></input>
+<a class="btn btn-primary mt-2"  role="button">Beitrag Löschen</a>
+<a class="btn btn-primary mt-2"  role="button">Änderungen verwerfen</a>
 </form>
+
+<?php
+
+?>
+<style media="screen">
+  textarea{
+    resize: none;
+  }
+</style>
--- blog_new.php
+++ blog_new.php
@@ -1,4 +1,37 @@
 <?php
+$title = "";
+$content  ="";
+$entry = null;
 
-echo "neuer blog";
- ?>
+if (isset($_POST["inputTitel"])) {
+  $title = $_POST["inputTitel"];
+}
+
+if (isset($_POST["inputContent"])) {
+  $content = $_POST["inputContent"];
+}
+if (strlen($title)>1) {
+addEntry($blogId, $title, $content);
+}
+?>
+
+<form action="<?php echo $_SERVER['PHP_SELF']."?function=blog_new"; ?>" method="post">
+
+  <div class="form-group" style="margin-top: 4em; width: 50%">
+    <label for="inputTitel">Titel:</label>
+    <textarea rows="1" type="text" name="inputTitel" id="inputTitel" placeholder="Titel" class="form-control"></textarea>
+<br></br>
+  <div  style="margin-top: 4em;  width: 50%;">
+    <label for="inputContent">Text:</label>
+    <br></br>
+    <textarea rows="35" cols="120" type="text" name="inputContent" id="inputContent" placeholder="Content"></textarea>
+  </div>
+  <br></br>
+<button type="submit" name="veröffentlichen">Beitrag veröffentlichen</button>
+</form>
+
+<style media="screen">
+  textarea{
+    resize: none;
+  }
+</style>
